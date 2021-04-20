04bfcc95e15a32029c5c08ce1fcc4b301f2c5b66
Robert French <rfrench@rfrench.org>
2021-04-20 10:38:02 -0700

    General cleanup of LEDArc

--- rob/main/led_arc_board.h
+++ rob/main/led_arc_board.h
@@ -3,7 +3,7 @@
  *
  * This class defines the interface for the serial LED Arc Board. There can
  * be a single board with 8 LEDs, or two boards with 16 LEDs. The LEDs are
- * RGB. The basic functionality is to draw an arc of LEDs from the low
+ * RGB. The basic functionality is to draw an arc of LEDs from the lowest
  * value to the high value. Optionally the moving average and/or peak value
  * can be displayed automatically.
  *
@@ -26,21 +26,39 @@
 #define LEDARC_CLOCK_PIN  8
 #define LEDARC_DATA_PIN   6
 
-#define LEDARC_DEFAULT_PWM 240 // 220
+#define LEDARC_DEFAULT_PWM 240 // 0-255, higher is dimmer
 
+// When moving the arc to a higher or lower value, optionally the arc can move
+// one LED at a time to make it look smoother. If this feature is enabled, this
+// is the delay between adjacent LEDs.
 #define LEDARC_DEFAULT_ARC_SMOOTHING_SPEED 20 // ms
 
+// If enabled, a moving average is calculated at a regular interval based on
+// the current arc value. This is the delay between samples used to compute the
+// moving average.
 #define LEDARC_DEFAULT_MOVING_AVERAGE_SPEED 100 // ms
+// The moving average is calculated by keeping samples in a round-robin buffer.
+// This is the maximum and default number of samples to keep. The moving average
+// is calculated over a time period of
+// LEDARC_DEFAULT_MOVING_AVERAGE_SPEED * LEDARC_MAX_AVERAGE_HISTORY
 #define LEDARC_MAX_AVERAGE_HISTORY 20 // Samples @ given interval
 #define LEDARC_DEFAULT_AVERAGE_HISTORY 20 // Samples @ given interval
 
+// If enabled, a "sticky" peak value is stored and displayed for the given
+// amount of time. If the arc value increases, the peak value is set to the new
+// value, but if the arc value decreases, the old peak value is maintained.
+// After the time has elapsed, the peak value is forgotten and a new peak is
+// stored.
 #define LEDARC_DEFAULT_PEAK_TTL 4000 // ms
 
 class LEDArcBoard
 {
   public:
     // The id is purely for external use if you need to identify which instance
-    // of the class you're looking at
+    // of the class you're looking at. It is not currently used for anything
+    // else.
+    // The bool arguments say whether or not to turn on the given feature.
+    // These can be changed in the future with the methods below.
     LEDArcBoard(byte id,
                 bool enable_moving_average=true,
                 bool enable_peak=true,
@@ -48,56 +66,71 @@ class LEDArcBoard
     byte get_id();
     byte set_id(byte id);
 
-    // Start operation
+    // Initialize operation. Must be called before calling update_leds the
+    // first time.
     void begin();
 
-    // Update the LEDs with the current values
-    // Until this is called nothing changes on the hardware
+    // Update the LEDs with the current value (and moving average and peak).
+    // Until this is called nothing changes on the hardware. This method
+    // should be called as often as possible. If the proper time hasn't elapsed,
+    // nothing will be done.
     void update_leds();
 
-    void set_current_val(byte cur_val);
-    byte get_current_val();
+    // Set the current value to display on the arc. This value is in "source
+    // units", which will be mapped later in some manner to the particular LED
+    // positions.
+    void set_current_val(int16_t cur_val);
+    uint16_t get_current_val();
 
-    void enable_arc_smoothing();
-    void disable_arc_smoothing();
-    void set_arc_smoothing_speed(unsigned int);
-    unsigned int get_arc_smoothing_speed();
-
+    // Change the LED brightness (0-255). Higher values are dimmer.
     void set_led_brightness(byte pwm);
     byte get_led_brightness();
 
+    // Arc smoothing gradually adds/removes one LED at a time when the displayed
+    // value changes.
+    void enable_arc_smoothing();
+    void disable_arc_smoothing();
+    void set_arc_smoothing_speed(unsigned long);
+    unsigned long get_arc_smoothing_speed();
+
+    // The moving average calculates the average and displays it as a special
+    // color. See comments above for details.
     void enable_moving_average();
     void disable_moving_average();
-    void set_moving_average_size(byte size);
-    void set_moving_average_speed(unsigned int);
-    unsigned int get_moving_average_speed();
+    void set_moving_average_size(uint16_t size);
+    uint16_t get_moving_average_size();
+    void set_moving_average_speed(unsigned long);
+    unsigned long get_moving_average_speed();
 
+    // The peak displays a "sticky" peak value which eventually disappears.
+    // See comments above for details.
     void enable_peak();
     void disable_peak();
-    void set_peak_ttl(unsigned int ttl);
+    void set_peak_ttl(unsigned long ttl);
+    unsigned long get_peak_ttl();
 
   private:
     byte _convert_val_to_index(byte val);
 
     byte _id; // Unique ID of this board
-    byte _cur_val; // The most recently set value to display
+    int16_t _cur_val; // The most recently set value to display
     byte _displayed_index; // The index of the highest LED actually displayed
     bool _smooth_arc; // True if we want to smooth out changes in the arc
-    unsigned int _arc_smoothing_speed; // Number of ms between arc changes
+    unsigned long _arc_smoothing_speed; // Number of ms between arc changes
     unsigned long _last_led_update_time; // The time of the last arc update
     byte _brightness; // The PWM of the LEDs
     byte _last_brightness; // So we don't update the pin unnecessarily
 
     bool _ave_enabled; // True if we want to keep a moving average
     byte _ave_val_history[LEDARC_MAX_AVERAGE_HISTORY]; // Round robin buffer
-    byte _ave_history_pos; // Position in buffer
-    byte _ave_history_size; // How much of the buffer we actually want to use
-    unsigned int _ave_speed; // Number of ms between updates to the buffer
+    uint16_t _ave_history_pos; // Position in buffer
+    uint16_t _ave_history_size; // How much of the buffer we actually want to use
+    unsigned long _ave_speed; // Number of ms between updates to the buffer
     unsigned long _last_ave_time; // The time of the last buffer update
 
     bool _peak_enabled; // True if we want to have a maximum-value LED
     byte _peak_index; // The index of the peak LED
-    unsigned int _peak_ttl; // How long the peak LED stays lit (ms)
+    unsigned long _peak_ttl; // How long the peak LED stays lit (ms)
     unsigned long _last_peak_update_time; // The time of the last update of the peak LED
 };
 
--- rob/main/led_arc_board.ino
+++ rob/main/led_arc_board.ino
@@ -60,6 +60,10 @@ void LEDArcBoard::begin()
   digitalWrite(LEDARC_STORE_PIN, HIGH);
 }
 
+//
+// General accessors for private member variables
+//
+
 // The ID is used in case you want to instantiate more than one LEDArcBoard
 // and need an easy way to distinguish among them
 byte LEDArcBoard::set_id(byte id)
@@ -72,35 +76,19 @@ byte LEDArcBoard::get_id()
   return _id;
 }
 
-void LEDArcBoard::set_current_val(byte cur_val)
+//
+
+void LEDArcBoard::set_current_val(int16_t cur_val)
 {
   _cur_val = cur_val;
 }
 
-byte LEDArcBoard::get_current_val()
+int16_t LEDArcBoard::get_current_val()
 {
   return _cur_val;
 }
 
-void LEDArcBoard::enable_arc_smoothing()
-{
-  _smooth_arc = true;
-}
-
-void LEDArcBoard::disable_arc_smoothing()
-{
-  _smooth_arc = false;
-}
-
-void LEDArcBoard::set_arc_smoothing_speed(unsigned int speed)
-{
-  _arc_smoothing_speed = speed;
-}
-
-unsigned int LEDArcBoard::get_arc_smoothing_speed()
-{
-  return _arc_smoothing_speed;
-}
+//
 
 void LEDArcBoard::set_led_brightness(byte pwm)
 {
@@ -112,6 +100,30 @@ byte LEDArcBoard::get_led_brightness()
   return _brightness;
 }
 
+//
+
+void LEDArcBoard::enable_arc_smoothing()
+{
+  _smooth_arc = true;
+}
+
+void LEDArcBoard::disable_arc_smoothing()
+{
+  _smooth_arc = false;
+}
+
+void LEDArcBoard::set_arc_smoothing_speed(unsigned long speed)
+{
+  _arc_smoothing_speed = speed;
+}
+
+unsigned long LEDArcBoard::get_arc_smoothing_speed()
+{
+  return _arc_smoothing_speed;
+}
+
+//
+
 void LEDArcBoard::enable_moving_average()
 {
   _ave_enabled = true;
@@ -122,22 +134,31 @@ void LEDArcBoard::disable_moving_average()
   _ave_enabled = false;
 }
 
-void LEDArcBoard::set_moving_average_size(byte size)
+void LEDArcBoard::set_moving_average_size(uint16_t size)
 {
     _ave_history_size = size;
+    // When changing the size, we start the moving average over from the
+    // beginning to make sure we're not running off the end of the new buffer
     memset(_ave_val_history, 0, sizeof(_ave_val_history));
     _ave_history_pos = 0;
 }
 
-void LEDArcBoard::set_moving_average_speed(unsigned int speed)
+uint16_t LEDArcBoard::get_moving_average_size()
+{
+  return _ave_history_size;
+}
+
+void LEDArcBoard::set_moving_average_speed(unsigned long speed)
 {
   _ave_speed = speed;
 }
 
-unsigned int LEDArcBoard::get_moving_average_speed()
+unsigned long LEDArcBoard::get_moving_average_speed()
 {
   return _ave_speed;
 }
+
+//
 
 void LEDArcBoard::enable_peak()
 {
@@ -149,10 +170,166 @@ void LEDArcBoard::disable_peak()
   _peak_enabled = false;
 }
 
-void LEDArcBoard::set_peak_ttl(unsigned int ttl)
+void LEDArcBoard::set_peak_ttl(unsigned long ttl)
 {
   _peak_ttl = ttl;
 }
+
+unsigned long LEDArcBoard::get_peak_ttl()
+{
+  return _peak_ttl;
+}
+
+//
+// Start of code to actually manipulate the LEDs
+//
+
+// Convert a "source value" to a LED index. This allows the "source value"
+// to be in whatever units the caller wants, and we only pay attention to LED
+// index internally.
+byte LEDArcBoard::_convert_val_to_index(int16_t val)
+{
+  // This is a placeholder for now until I figure out how I want to convert
+  // S-meter values to index value
+  if (val > 16)
+    return 16;
+  return val;
+}
+
+// Convert an index (0=off, 1-8 or 1-16=LED number from low to high)
+// to a bitmask that has all the bits from the index down to 0 set.
+uint16_t _index_to_arc_bitmask(byte index)
+{
+  if (index >= 16) // Separate case since 1<<index might overflow on 16
+    return 0xffff;
+  else if (index > 0)
+    return (1 << index)-1;
+  return 0;
+}
+
+// Convert an index (0=off, 1-8 or 1-16=LED number from low to high)
+// to a bitmask with a single bit set for that index.
+uint16_t _index_to_single_bitmask(byte index)
+{
+  if (index > 0)
+    return 1 << (index-1);
+  return 0;
+}
+
+void LEDArcBoard::update_leds()
+{
+  unsigned long cur_milli = millis();
+  byte cur_index = _convert_val_to_index(_cur_val);
+
+  // If enabled, and the proper time has elapsed, update the moving average
+  // buffer
+  if (_ave_enabled)
+  {
+    // We might have to do this more than once if it has been a long time since
+    // the LEDs were last updated, so we play catch up here to make sure the
+    // moving average represents the proper number of samples (even if they're
+    // the same value over and over)
+    while (cur_milli >= _last_ave_time+_ave_speed)
+    {
+      _last_ave_time += _ave_speed;
+      // We store the historical values in a round robin buffer
+      _ave_val_history[_ave_history_pos] = _cur_val;
+      _ave_history_pos++;
+      if (_ave_history_pos >= _ave_history_size)
+          _ave_history_pos = 0;
+    }
+  }
+
+  // All updates to the LEDs, including the moving average and peak, happen
+  // at the rate of the arc smoothing speed, if smoothing is enabled.
+  if (_smooth_arc && cur_milli < _last_led_update_time+_arc_smoothing_speed)
+    return;
+  _last_led_update_time = cur_milli;
+
+  if (!_smooth_arc)
+    _displayed_index = cur_index;
+  else
+    // We "smooth" the arc by only adding or removing one LED at a time
+    if (cur_index > _displayed_index)
+      _displayed_index++;
+    else if (cur_index < _displayed_index)
+      _displayed_index--;
+
+  // We're starting with "0" meaning off just to make the bit manipulation
+  // prettier; we flip it later
+  uint16_t red = 0;
+  uint16_t green = 0;
+  uint16_t blue = 0;
+
+  // Make the current value blue in a complete arc
+  blue |= _index_to_arc_bitmask(_displayed_index);
+
+  // If enabled, calculate the actual moving average and mark it yellow
+  if (_ave_enabled)
+  {
+    long sum = 0; // Use long since sum can overflow a single int16
+    for (int i=0; i<_ave_history_size; i++)
+        sum += _ave_val_history[i];
+    // Computing the LED index uses integer division, which rounds down.
+    // It might be nicer to round up or down as appropriate.
+    byte ave_index = _convert_val_to_index(sum / _ave_history_size);
+
+    uint16_t bitmask = _index_to_single_bitmask(ave_index);
+    red |= bitmask;
+    green |= bitmask;
+    blue &= ~bitmask;
+  }
+
+  // If enabled, compute the new peak value and mark it white.
+  // We do this after average so the white overrides the yellow.
+  if (_peak_enabled)
+  {
+    if (_displayed_index >= _peak_index)
+    {
+      _peak_index = _displayed_index;
+      _last_peak_update_time = cur_milli;
+    }
+    else if (_peak_index != 0 && cur_milli >= _last_peak_update_time+_peak_ttl)
+        _peak_index = 0;
+
+    uint16_t bitmask = _index_to_single_bitmask(_peak_index);
+    red |= bitmask;
+    green |= bitmask;
+    blue |= bitmask;
+  }
+
+  // Left half of arc
+  byte red1   = ~(red & 0xff00) >> 8;
+  byte green1 = ~(green & 0xff00) >> 8;
+  byte blue1  = ~(blue & 0xff00) >> 8;
+  // Right half of arc
+  byte red2   = ~(red & 0xff);
+  byte green2 = ~(green & 0xff);
+  byte blue2  = ~(blue & 0xff);
+
+  if (_brightness != _last_brightness)
+  {
+    analogWrite(LEDARC_PWM_PIN, _brightness);
+    _last_brightness = _brightness;
+  }
+
+  digitalWrite(LEDARC_STORE_PIN, LOW);
+#ifdef LEDARC_16_RGB
+  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, MSBFIRST, red1);
+  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, MSBFIRST, green1);
+  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, MSBFIRST, blue1);
+#endif
+  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, LSBFIRST, red2);
+  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, LSBFIRST, green2);
+  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, LSBFIRST, blue2);
+  digitalWrite(LEDARC_STORE_PIN, HIGH);
+}
+
+
+
+
+
+
 
 #ifdef XXX
 byte LEDArcBoard::calculate_current_MAPPED_value(byte S_METER_from_RADIO)    // pass  s meter value (from radio ) in here
@@ -220,137 +397,3 @@ byte LEDArcBoard::calculate_current_MAPPED_value(byte S_METER_from_RADIO)    // 
 
 }
 #endif
-
-byte LEDArcBoard::_convert_val_to_index(byte val)
-{
-  if (val > 16)
-    return 16;
-  return val;
-}
-
-word _index_to_arc_bitmask(byte index)
-{
-  word ret = 0;
-  if (index >= 16)
-    ret = 0xffff;
-  else if (index > 0)
-    ret |= (1 << index)-1;
-  return ret;
-}
-
-word _index_to_single_bitmask(byte index)
-{
-  word ret = 0;
-  if (index > 0)
-    ret = 1 << (index-1);
-  return ret;
-}
-
-void LEDArcBoard::update_leds()
-{
-  unsigned long cur_milli = millis();
-  byte cur_index = _convert_val_to_index(_cur_val);
-
-  // If enabled, and the proper time has elapsed, update the moving average
-  if (_ave_enabled)
-  {
-    // We might have to do this more than once if it has been a long time since
-    // the LEDs were last updated, so we play catch up here to make sure the
-    // moving average represents the proper number of samples (even if they're
-    // the same value over and over)
-    while (cur_milli >= _last_ave_time+_ave_speed)
-    {
-      _last_ave_time += _ave_speed;
-      // We store the historical values in a round robin buffer
-      _ave_val_history[_ave_history_pos] = _cur_val;
-      _ave_history_pos++;
-      if (_ave_history_pos >= _ave_history_size)
-          _ave_history_pos = 0;
-    }
-  }
-
-  if (cur_milli < _last_led_update_time+_arc_smoothing_speed)
-    return;
-  _last_led_update_time = cur_milli;
-
-  if (!_smooth_arc)
-    _displayed_index = cur_index;
-  else
-    // We "smooth" the arc by only adding or removing one LED at a time
-    if (cur_index > _displayed_index)
-      _displayed_index++;
-    else if (cur_index < _displayed_index)
-      _displayed_index--;
-
-  // We're starting with "0" meaning off just to make the big manipulation
-  // prettier
-  word red = 0;
-  word green = 0;
-  word blue = 0;
-
-  // Make the current value blue in a complete arc
-  blue |= _index_to_arc_bitmask(_displayed_index);
-
-  // If enabled, calcualte the actual average and mark it yellow
-  if (_ave_enabled)
-  {
-    int sum = 0;
-    for (int i=0; i<_ave_history_size; i++)
-    {
-        sum += _ave_val_history[i];
-    }
-    byte ave_index = _convert_val_to_index(sum / _ave_history_size);
-
-    word bitmask = _index_to_single_bitmask(ave_index);
-    red |= bitmask;
-    green |= bitmask;
-    blue &= ~bitmask;
-  }
-
-  // If enabled, compute the new peak value and mark
-  // We do this after average so the white overrides the yellow
-  if (_peak_enabled)
-  {
-    if (_displayed_index >= _peak_index)
-    {
-      _peak_index = _displayed_index;
-      _last_peak_update_time = cur_milli;
-    }
-    else if (_peak_index != 0)
-    {
-      if (cur_milli >= _last_peak_update_time+_peak_ttl)
-        _peak_index = 0;
-    }
-
-    word bitmask = _index_to_single_bitmask(_peak_index);
-    red |= bitmask;
-    green |= bitmask;
-    blue |= bitmask;
-  }
-
-  // Left half of arc
-  byte red1   = ~(red & 0xff00) >> 8;
-  byte green1 = ~(green & 0xff00) >> 8;
-  byte blue1  = ~(blue & 0xff00) >> 8;
-  // Right half of arc
-  byte red2   = ~(red & 0xff);
-  byte green2 = ~(green & 0xff);
-  byte blue2  = ~(blue & 0xff);
-
-  if (_brightness != _last_brightness)
-  {
-    analogWrite(LEDARC_PWM_PIN, _brightness);
-    _last_brightness = _brightness;
-  }
-
-  digitalWrite(LEDARC_STORE_PIN, LOW);
-#ifdef LEDARC_16_RGB
-  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, MSBFIRST, red1);
-  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, MSBFIRST, green1);
-  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, MSBFIRST, blue1);
-#endif
-  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, LSBFIRST, red2);
-  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, LSBFIRST, green2);
-  shiftOut(LEDARC_DATA_PIN, LEDARC_CLOCK_PIN, LSBFIRST, blue2);
-  digitalWrite(LEDARC_STORE_PIN, HIGH);
-}
